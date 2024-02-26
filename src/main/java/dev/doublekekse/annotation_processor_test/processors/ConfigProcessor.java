4ef88aa4eb6239466be991eb8327208cdccbbe44
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2024-02-26 21:20:54 +0100

    cursed

--- src/main/java/dev/doublekekse/annotation_processor_test/processors/ConfigProcessor.java
+++ src/main/java/dev/doublekekse/annotation_processor_test/processors/ConfigProcessor.java
@@ -8,25 +8,50 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Set;
 
-@SupportedAnnotationTypes("Config")
+@SupportedAnnotationTypes("dev.doublekekse.annotation_processor_test.Config")
 @SupportedSourceVersion(SourceVersion.RELEASE_17)
 public class ConfigProcessor extends AbstractProcessor {
-
+    public static final String TEMPLATE = """
+        package %s;
+        
+        class %s {
+            toString() {
+                return "O: %s, N: %s"
+            }
+        }
+        """;
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         for (Element element : roundEnv.getElementsAnnotatedWith(Config.class)) {
             if (element instanceof TypeElement typeElement) {
+                if (typeElement.getKind() != ElementKind.CLASS) continue;
 
-                // Generate code for the save and load methods
-                generateSaveLoadMethods(typeElement);
+                var configName = typeElement.getAnnotation(Config.class).configWrapperName();
+                var modelName = typeElement.getQualifiedName();
+
+                try {
+                    var file = this.processingEnv.getFiler().createSourceFile(configName);
+                    try (var writer = new PrintWriter(file.openWriter())) {
+                        writer.println(buildConfig(configName, modelName.toString()));
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException("Failed to generate config", e);
+                }
             }
         }
         return true;
+    }
+
+    public String buildConfig(String configName, String modelName) {
+        var pacakge = modelName.substring(0, modelName.lastIndexOf("."));
+
+        return String.format(TEMPLATE, pacakge, configName, modelName, configName);
     }
 
     private void generateSaveLoadMethods(TypeElement typeElement) {
@@ -55,7 +80,7 @@ public class ConfigProcessor extends AbstractProcessor {
 
             // Create a new source file for the generated methods
             try (PrintWriter writer = new PrintWriter(
-                processingEnv.getFiler().createSourceFile(packageName + ".Generated" + className).openWriter())) {
+                processingEnv.getFiler().createSourceFile(packageName + "Generated" + className).openWriter())) {
 
                 // Add the package declaration
                 if (!packageName.isEmpty()) {
