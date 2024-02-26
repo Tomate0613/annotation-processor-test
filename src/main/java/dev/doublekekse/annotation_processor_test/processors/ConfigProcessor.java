636bda232b18db7360545a752fe112c55e03e4ea
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2024-02-26 20:40:13 +0100

    a

--- src/main/java/dev/doublekekse/annotation_processor_test/processors/ConfigProcessor.java
+++ src/main/java/dev/doublekekse/annotation_processor_test/processors/ConfigProcessor.java
@@ -14,6 +14,7 @@ import java.io.PrintWriter;
 import java.util.Set;
 
 @SupportedAnnotationTypes("Config")
+@SupportedSourceVersion(SourceVersion.RELEASE_17)
 public class ConfigProcessor extends AbstractProcessor {
 
     @Override
